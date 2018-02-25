27fe28e0e09d709b4a91661a4472befa92dd5efe
Khalilur Rahman <khal.rahman@accedo.tv>
2018-02-25 19:37:51 +0800

    Fixed e2e message

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('dragon-battle App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to the Battle of Dragon!');
   });
 });
